f51bef995f72cbfdc935bca164a6bf0e2374e76e
ca1c <gegs921@gmail.com>
2023-07-11 19:34:03 -0700

    bee rotates towards target coordinate

--- core/src/com/badassbees/game/Bee.java
+++ core/src/com/badassbees/game/Bee.java
@@ -14,48 +14,121 @@ public class Bee {
     public float topSpeed = 500f;
     public float xSpeed = 0f;
     public float ySpeed = 500f;
-    public float rotationSpeed = 500f;
+    public float rotationSpeed = 200f;
     public float rotationAngle = 360f;
+    public float moveAngle;
+    public enum Quadrant {
+        FIRST,
+        SECOND,
+        THIRD,
+        FOURTH;
+    }
+
     public Bee(Texture img) {
         sprite = new Sprite(img);
-        position = new Vector2(Gdx.graphics.getWidth()/2f,sprite.getScaleY()*sprite.getHeight()/2);
         sprite.setScale(2);
+        position = new Vector2(Gdx.graphics.getWidth()/2 - sprite.getWidth(),
+                Gdx.graphics.getHeight()/2 - sprite.getHeight());
+    }
+
+    public boolean beeHittingRightWall() {
+        return position.x + sprite.getWidth() > Gdx.graphics.getWidth();
+    }
+
+    public boolean beeHittingLeftWall() {
+        return position.x < 0;
+    }
+
+    public boolean beeHittingTopWall() {
+        return position.y + sprite.getHeight() > Gdx.graphics.getHeight();
+    }
+
+    public boolean beeHittingBottomWall() {
+        return position.y < 0;
+    }
+
+    public void updateBeeCoordinates(float deltaTime) {
+        position.x += deltaTime * xSpeed;
+        position.y += deltaTime * ySpeed;
+    }
+
+    public void checkBeeHittingBorders() {
+        if(beeHittingRightWall()) {
+            position.x -= sprite.getWidth();
+        }
+        if(beeHittingLeftWall()) {
+            position.x += sprite.getWidth();
+        }
+        // Check if bee is hitting top and bottom walls
+        if(beeHittingTopWall()) {
+            position.y -= sprite.getHeight();
+        }
+        if(beeHittingBottomWall()) {
+            position.y += sprite.getHeight();
+        }
+    }
+
+    // gets quadrant of coordinate
+    public float getCoordinateQuadrantDivisor(float x, float y) {
+        float yAxis = Gdx.graphics.getWidth() / 2;
+        float xAxis = Gdx.graphics.getHeight() / 2;
+
+        if(x > yAxis && y > xAxis) {
+            return 90f;
+        }
+        else if(x > yAxis && y < xAxis) {
+            return 180f;
+        }
+        else if(x < yAxis && y < xAxis) {
+            return 270f;
+        }
+        else {
+            return 360f;
+        }
+    }
+
+    // gets the angle bee needs to be rotated to, to move to a certain coordinate
+    public float getPathfindingRotationAngle(float x1, float y1, float x2, float y2) {
+        float c = (y2 - y1) / (x2 - x1);
+        float divisor = getCoordinateQuadrantDivisor(x1, y1);
+        System.out.println(divisor);
+        float theta = divisor - MathUtils.atanDeg(c);
+
+        return theta;
+    }
+
+    public float targetAngle() {
+        return 360 - getPathfindingRotationAngle(100, 100, position.x, position.y);
     }
 
     public void moveForward(float deltaTime) {
         if(Gdx.input.isKeyPressed(Input.Keys.W)) {
-            position.x += deltaTime * xSpeed;
-            position.y += deltaTime * ySpeed;
-            // Check if bee is hitting side walls
-            if(position.x + sprite.getWidth() > Gdx.graphics.getWidth()) {
-                position.x -= sprite.getWidth();
-            }
-            if(position.x < 0) {
-                position.x += sprite.getWidth();
-            }
-            // Check if bee is hitting top and bottom walls
-            if(position.y + sprite.getHeight() > Gdx.graphics.getHeight()) {
-                position.y -= sprite.getHeight();
-            }
-            if(position.y < 0) {
-                position.y += sprite.getHeight();
-            }
+            moveAngle = targetAngle();
+            updateBeeCoordinates(deltaTime);
+            checkBeeHittingBorders();
         }
     }
 
+    public void rotateLeft(float deltaTime) {
+        rotationAngle += deltaTime*rotationSpeed;
+        if(rotationAngle > 360) {
+            rotationAngle -= 360;
+        }
+    }
+
+    public void rotateRight(float deltaTime) {
+        rotationAngle += 360 - deltaTime*rotationSpeed;
+        if(rotationAngle > 360) {
+            rotationAngle -= 360;
+        }
+    }
 
     public void updateRotationAngle(float deltaTime) {
         if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            rotationAngle += deltaTime*rotationSpeed;
-            if(rotationAngle > 360) {
-                rotationAngle -= 360;
-            }
+            rotateLeft(deltaTime);
         }
         if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            rotationAngle += 360 - deltaTime*rotationSpeed;
-            if(rotationAngle > 360) {
-                rotationAngle -= 360;
-            }
+            rotateRight(deltaTime);
         }
     }
 
@@ -92,11 +165,22 @@ public class Bee {
     }
 
     public void Update(float deltaTime) {
+
+        float myAngle = targetAngle();
+//        System.out.println(rotationAngle + " " + myAngle);
+        if(rotationAngle > myAngle) {
+//            System.out.println(myAngle);
+            rotateRight(deltaTime);
+        }
+        if(rotationAngle < myAngle) {
+            rotateLeft(deltaTime);
+        }
+
         moveForward(deltaTime);
         updateRotationAngle(deltaTime);
         updateVelocity();
     }
-
+    int runs = 0;
     public void Draw(SpriteBatch batch) {
         Update(Gdx.graphics.getDeltaTime());
         sprite.setPosition(position.x, position.y);
